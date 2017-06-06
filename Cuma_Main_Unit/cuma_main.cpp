ea18ed9529510774baf927b08bea3dcbcb8f6fa0
laskdjlaskdj12 <laskdjlaskdj12@gmail.com>
2017-06-07 01:41:12 +0900

    1. cuma_main_unit에서 send, recv의 시그널 전달 방식을 네트워크 소켓처럼 본뜸

--- Cuma_Main_Unit/cuma_main.cpp
+++ Cuma_Main_Unit/cuma_main.cpp
@@ -6,3 +6,53 @@ Cuma_Main::Cuma_Main(QObject *parent) : QObject(parent),
 {
     
 }
+
+Cuma_Main::~Cuma_Main()
+{
+
+}
+
+void Cuma_Main::read_file_name(QString f)
+{
+
+}
+
+void Cuma_Main::set_timeout(uint32_t timeout)
+{
+
+}
+
+void Cuma_Main::copy_unit_list(QList<QSharedPointer<Cuma_Main> > list)
+{
+
+}
+
+QJsonObject Cuma_Main::get_result_json()
+{
+
+}
+
+void Cuma_Main::send_ping_message_sl()
+{
+
+}
+
+void Cuma_Main::recv_ping_message_sl()
+{
+
+}
+
+void Cuma_Main::send_file_message_sl_out(QByteArray binary)
+{
+
+}
+
+void Cuma_Main::recv_file_message_sl_in(QByteArray binary)
+{
+
+}
+
+void Cuma_Main::stop_simulation_sl()
+{
+
+}
--- Cuma_Main_Unit/cuma_main.h
+++ Cuma_Main_Unit/cuma_main.h
@@ -28,37 +28,51 @@ public:
     //유닛 리스트들을 복사함
     void copy_unit_list(QList<QSharedPointer<Cuma_Main>> list);
 
+    //전송된 유닛들의 시간들의 리포트를 json으로 리턴함
     QJsonObject get_result_json();
 
     //핑 메세지
 signals:
-    void send_ping_message_si();
-    void recv_ping_message_si();
+
+    /*
+     * 중요 : 반드시 send, recv의 시그널들은 네트워크 소켓을 본따 만들어졌음
+     *  ex) send -> 자기가 다른 유닛에게 전달할때
+     *  자기 유닛 안에 있는 send_out을 emit해서 바이너리를 전달하는 방식
+     *  그리고 받는 유닛에서는 recv_in의 시그널을 받아서 connect된 slot을 작동하게 하는 방식
+     *
+     * 1. 자기를 기준으로 다른 유닛에게 전송하는것을 out으로 함
+     * 2. 다른 유닛이 자기 시그널을 emit하는것을 in으로함
+     * */
+
+    void send_ping_message_si_out();
+    void recv_ping_message_si_in();
 
     //파일 메세지
-    void send_file_message_si(QByteArray);
-    void recv_file_message_si(QByteArray);
+    void send_file_message_si_out(QByteArray);
+    void recv_file_message_si_in(QByteArray);
 
-    //메인에서 강제로 종료했을시 리턴함
-    void quit_si();
+    //attach된 쓰레드에 끝났다는것을 리턴함
+    void quit_si_int_out();
+
+    //메인에서 stop시그널이 왔을경우
+    void stop_si_in();
  
     //내부에서 메세지 프로시저
 public slots:
-    void send_ping_message_sl();
-    void recv_ping_message_sl();
 
-    void send_file_message_sl(QByteArray binary);
-    void recv_file_message_sl(QByteArray binary);
+    void send_ping_message_sl_out();
+    void recv_ping_message_sl_in();
 
-    //쓰레드가 끝났다는것을 리턴함
-    void stop_simulation();
+    void send_file_message_sl_out(QByteArray binary);
+    void recv_file_message_sl_in(QByteArray binary);
+
+    //메인에서 stop시그널이 왔을경우
+    void stop_simulation_sl();
 
 private:
     
-    //파일 read
-    bool read_file();
-    
 private:
+    QList<QByteArray> file_frag;
     
     //Cuma_Main의 타겟설정
     QSharedPointer<Cuma_Main> target;
