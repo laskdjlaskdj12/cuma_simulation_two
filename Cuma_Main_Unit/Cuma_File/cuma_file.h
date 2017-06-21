68e2c38f3920b4e4e85b145d25b7e7d6361cd6de
laskdjlaskdj12 <laskdjlaskdj12@gmail.com>
2017-06-22 05:52:51 +0900

    1. Cuma_File를 클래시 Re_Design함

--- Cuma_Main_Unit/Cuma_File/cuma_file.h
+++ Cuma_Main_Unit/Cuma_File/cuma_file.h
@@ -67,4 +67,32 @@ private:
     unsigned long _file_frag_size;
 };
 
+class Cuma_File_ReFectoring: public QObject
+{
+    Q_OBJECT
+public:
+    explicit Cuma_File_ReFectoring(QObject* parent = 0);
+    ~Cuma_File_ReFectoring();
+
+    virtual void set_File_Frag_Count(uint32_t c);
+    virtual void set_File_Name(QString n);
+    virtual void save_File_Frag(QVector<QByteArray> frag, QString name);
+    virtual void save_File_Frag(QVector<QByteArray> frag, QString name, uint32_t index);
+
+    virtual uint32_t get_File_Index();
+    virtual QString get_File_Name();
+    virtual QVector<QByteArray> get_File_Frag();
+    virtual QByteArray get_File_Frag_By_Index(uint32_t index);
+
+protected:
+    virtual int read_file();
+    virtual int make_frag();
+    virtual int save_Frag(QVector<QByteArray> frag, QString name, uint32_t index = NULL);
+
+private:
+    QString File_name;
+    uint32_t File_frag;
+    QVector<QByteArray> File_Frag;
+};
+
 #endif // CUMA_FILE_H
